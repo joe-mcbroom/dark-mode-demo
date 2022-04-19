fd57f41668d9fa987bbb9866751ec3df1366db5f
Joseph McBroom <josephmcbroom@wtmbpjoemcbroom.lan>
2022-04-19 16:20:33 -0400

    4️⃣  Use onMounted to get initial theme and incorporate localStorage

--- src/components/DarkSelector.vue
+++ src/components/DarkSelector.vue
@@ -1,11 +1,24 @@
 <script setup>
-import { ref } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 
 const isDark = ref(false);
 
+const body = ref(null);
+
+// Set the local ref and update local storage
 const setDarkMode = (value) => {
 	isDark.value = value;
+	localStorage.setItem('theme', value ? 'dark' : '');
 };
+
+onMounted(() => {
+	// Check if the local storage is set or if user prefers dark
+	const prefersDark =
+		localStorage.getItem('theme') === 'dark' ||
+		(!('theme' in localStorage) &&
+			window.matchMedia('(prefers-color-scheme: dark)').matches);
+	setDarkMode(prefersDark);
+});
 </script>
 <template>
 	<div class="select-none cursor-pointer" @click="setDarkMode(!isDark)">
